bb405814c1dec97d7097f2fc282a1f1c0203a309
Tadas <tadas@brand42.co.uk>
2019-10-07 17:54:53 +0100

    changes to text

--- src/app/screens/MicroPlasticScreen.js
+++ src/app/screens/MicroPlasticScreen.js
@@ -407,10 +407,8 @@ export default class MicroPlasticScreen extends BaseScreen {
               year</h4>
             <div className="Paragraph">
               <div className="Paragraph__content Paragraph--v2">
-                So, some of the world’s brightest minds have been focusing on tackling the issue and pressure is growing
-                for governments to act. But on a more personal level, what can be done to tackle the problem? There’s an
-                obvious answer – buy fewer plastic products and avoid shower gels and washing detergents that have
-                microbeads in them.
+                So, some of the world’s brightest minds have been focusing on tackling the issue and pressure is growing for governments to act. But on a more personal level, what can be done to tackle the problem?<br/><br/>
+                Dilyana Mihaylova, marine plastics manager from Fauna and Flora International, advises consumers to check the labels on their cosmetics to ensure that they do not contain plastics that can end up in reiver and the sea.
               </div>
             </div>
           </div>
